87b4da09705044d03b97bbed5c1039cb4703c6ec
Jeff Araujo <jaraujo@pipersolutions.com>
2021-09-06 00:09:15 -0500

    HTML y CSS funcionando

--- lab05/js/lab05.js
+++ lab05/js/lab05.js
@@ -1,1 +1,11 @@
+ function extraerTexto(){
+    let nomap = document.getElementById("nomap").value;
+    let arreglo = nomap.split(' ');
+    console.log(arreglo);
+    let apepa = arreglo[0];
+    console.log(apepa);
+
+    document.getElementById("appa").innerHTML = ` ${apepa}`
+ }
+ 
  
